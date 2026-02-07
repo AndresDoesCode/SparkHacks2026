caa786434505c4236ece6652e3f4758ee869fc57
Andres <hospitablepro1@gmail.com>
2026-02-06 19:53:33 -0600

    Basic things

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,13 +3,28 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  //cool = var
+  //setCool = how you change the variable
+  //useState = 
+  const [cool, setCool] = useState(0)
 
+  function evenBetter() {
+    setCool(prev => prev + 1)
+  }
 
   return (
     <>
+    <div className='background'>
+      <p>{cool}</p>
+      <button onClick={(evenBetter)}>Click me</button>
+      <p className="cool_thing">
+        geygvhcgyevcguyevcegec hello world of html
+      </p>
+    </div>
 
     </>
   )
+
 }
 
 export default App
